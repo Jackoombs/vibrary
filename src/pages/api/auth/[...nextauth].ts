7df8cec8b9bc053c1ed08ebe14f6f221f5304065
Jack Coombs <jack.coombs@hotmail.co.uk>
2022-11-22 09:53:22 +0000

    Created default shelves on acount creation.

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,58 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github"
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import { env } from "../../../env/server.mjs";
+import { prisma } from "../../../server/db/client";
+
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          shelves: {
+            create: [
+              { 
+                name: "All Books",
+                isDefault: true
+              },
+              { 
+                name: "Read",
+                isDefault: true
+              },              { 
+                name: "Not Yet Read",
+                isDefault: true
+              },
+            ]
+          }
+        }
+      },
+    }),
+    GitHubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
