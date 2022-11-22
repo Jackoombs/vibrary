f795f75492166d3e504eaeef49f430126e1e6c77
Jack Coombs <jack.coombs@hotmail.co.uk>
2022-11-22 09:49:44 +0000

    Added default font and basic css.

---
+++ src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type NextFontWithVariable } from "@next/font/dist/types";
+import { IBM_Plex_Sans } from "@next/font/google";
+
+import { trpc } from "../utils/trpc";
+
+import "../styles/globals.css";
+
+const ibm: NextFontWithVariable = IBM_Plex_Sans({
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-ibm",
+});
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <div className={ibm.variable + " min-h-screen bg-rose-200 text-stone-700 font-sans"}>
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
+  );
+};
+
+export default trpc.withTRPC(MyApp);
