04282610bddc8c40b07aa3fe128d3bf0b65d6d1e
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-17 22:41:50 +0000

    Prettier changes and small style changes.

--- src/components/Shelf/ShelfMenuItem.tsx
+++ src/components/Shelf/ShelfMenuItem.tsx
@@ -1,43 +1,60 @@
-import clsx from "clsx"
-import { ImBin } from "react-icons/im"
+import clsx from "clsx";
+import { ImBin } from "react-icons/im";
 
 interface Props {
-  name: string
-  count: number
-  shelfName: string
-  showDelete: boolean
-  setShelfName: React.Dispatch<React.SetStateAction<string>>
-  isHover: boolean
-  setDeleteModalOpen: React.Dispatch<React.SetStateAction<string>>
+  name: string;
+  count: number;
+  shelfName: string;
+  showDelete: boolean;
+  setShelfName: React.Dispatch<React.SetStateAction<string>>;
+  isHover: boolean;
+  setDeleteModalOpen: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const ShelfMenuItem = ({ name, count, shelfName, showDelete, setShelfName, setDeleteModalOpen, isHover }: Props) => {
-
-  return(
+const ShelfMenuItem = ({
+  name,
+  count,
+  shelfName,
+  showDelete,
+  setShelfName,
+  setDeleteModalOpen,
+  isHover,
+}: Props) => {
+  return (
     <li className="relative flex items-center justify-between">
-
-      <p      
-        className={clsx("z-10 relative flex items-center py-1 ml-3 px-3 w-max rounded-lg duration-150 text-ellipsis",
-          shelfName === name ? "text-primary cursor-default" : "cursor-pointer text-secondary",
-          "hover:text-primary hover:bg-secondary"
+      <p
+        className={clsx(
+          "relative z-10 ml-3 flex w-max items-center text-ellipsis rounded-lg py-1 px-3 font-medium tracking-widest duration-150",
+          shelfName === name
+            ? "cursor-default text-primary"
+            : "cursor-pointer text-secondary",
+          "hover:bg-secondary hover:text-primary"
         )}
-        onClick={() => setShelfName(name)}>
+        onClick={() => setShelfName(name)}
+      >
         {name} ({count})
-        <div 
-          className={clsx("-z-10 absolute top-0 right-0 w-[180%] h-full rounded-lg bg-secondary hover:block",
-          shelfName === name ? "block" : "hidden")}>
-        </div>
+        <span
+          className={clsx(
+            "absolute top-0 right-0 -z-10 h-full w-[180%] rounded-lg bg-secondary hover:block",
+            shelfName === name ? "block" : "hidden"
+          )}
+        ></span>
       </p>
-      {( showDelete ) &&
-        <button 
-          className="flex items-center justify-center w-8 h-8 cursor-pointer rounded-full duration-150 hover:bg-secondary hover:text-primary"
-          onClick={() => setDeleteModalOpen(name)}>
-          <ImBin 
-            className={clsx("duration-150", !isHover ? "opacity-0" : "opacity-100")} 
+      {showDelete && (
+        <button
+          className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-lg duration-150 hover:bg-secondary hover:text-primary"
+          onClick={() => setDeleteModalOpen(name)}
+        >
+          <ImBin
+            className={clsx(
+              "duration-150",
+              !isHover ? "opacity-0" : "opacity-100"
+            )}
           />
-        </button>}
+        </button>
+      )}
     </li>
-  )
-}
+  );
+};
 
-export default ShelfMenuItem
+export default ShelfMenuItem;
