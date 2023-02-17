fbaae6be76e18e4e3be1ff9f97346d7dc2332d0e
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-17 22:36:56 +0000

    Updated schema to include 'read' property.

--- prisma/migrations/20230217142307_new_migration/migration.sql
+++ prisma/migrations/20230217142307_new_migration/migration.sql
@@ -51,6 +51,7 @@ CREATE TABLE "Book" (
     "titleColor" TEXT NOT NULL,
     "pageCount" INTEGER,
     "publicationDate" TEXT,
+    "read" BOOLEAN NOT NULL,
     "userId" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT "Book_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
