afdf9517bcc61ce8f123729c11c05eecd001c35d
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-17 22:44:08 +0000

    Prettier update.

--- src/types/book.ts
+++ src/types/book.ts
@@ -1,10 +1,10 @@
 export type BookType = {
-  title: string
-  author?: string 
-  id: string
-  imageSrc: string
-  publicationDate?: string
-  pageCount?: number
-  spineColor: string
-  titleColor: string
-}
+  title: string;
+  author?: string;
+  id: string;
+  imageSrc: string;
+  publicationDate?: string;
+  pageCount?: number;
+  spineColor: string;
+  titleColor: string;
+};
