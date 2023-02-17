33de6899b53b77caa59b988c8c0b566e7c5fd9d0
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-02-17 22:43:55 +0000

    Added crud procedures.

--- src/server/trpc/router/book.ts
+++ src/server/trpc/router/book.ts
@@ -1,19 +1,21 @@
-import { z } from "zod";
+import { string, z } from "zod";
 import { router, publicProcedure } from "../trpc";
 
 export const bookRouter = router({
   add: publicProcedure
-    .input(z.object({
-      title: z.string(),
-      bookId: z.string(),
-      author: z.string().optional() ,
-      imageSrc: z.string(),
-      pageCount: z.number().optional(),
-      publicationDate: z.string().optional(),
-      shelfName: z.string()
-    }))
-    .mutation(({ctx,  input }) => {
-      if (ctx.session?.user?.id){
+    .input(
+      z.object({
+        title: z.string(),
+        bookId: z.string(),
+        author: z.string().optional(),
+        imageSrc: z.string(),
+        pageCount: z.number().optional(),
+        publicationDate: z.string().optional(),
+        shelfName: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      if (ctx.session?.user?.id) {
         return ctx.prisma.book.create({
           data: {
             userId: ctx.session.user.id,
@@ -25,15 +27,81 @@ export const bookRouter = router({
             publicationDate: input.publicationDate,
             spineColor: "#0f234e",
             titleColor: "#f9f0ee",
+            read: false,
             shelves: {
               connect: [
-                {name: input.shelfName},
-                {name: "All Books"}
-              ]
-            }
-          }
-        })
+                { name: input.shelfName },
+                { name: "All Books" },
+                { name: "Not Yet Read" },
+              ],
+            },
+          },
+        });
       }
-    
-    })
-}) 
+    }),
+  delete: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      if (ctx.session?.user?.id) {
+        return ctx.prisma.book.delete({
+          where: {
+            id: input.id,
+          },
+        });
+      }
+    }),
+  update: publicProcedure
+    .input(
+      z.object({
+        id: string(),
+        title: z.string().optional(),
+        author: z.string().optional(),
+        imageSrc: z.string().optional(),
+        spineColor: z.string().optional(),
+        titleColor: z.string().optional(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      if (ctx.session?.user?.id) {
+        return ctx.prisma.book.update({
+          where: {
+            id: input.id,
+          },
+          data: {
+            title: input.title,
+            author: input.author,
+            imageSrc: input.imageSrc,
+            spineColor: input.spineColor,
+            titleColor: input.titleColor,
+          },
+        });
+      }
+    }),
+  updateRead: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        read: z.boolean(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      if (ctx.session?.user?.id) {
+        return ctx.prisma.book.update({
+          where: {
+            id: input.id,
+          },
+          data: {
+            read: input.read,
+            shelves: {
+              connect: [{ name: input.read ? "Read" : "Not Yet Read" }],
+              disconnect: [{ name: !input.read ? "Read" : "Not Yet Read" }],
+            },
+          },
+        });
+      }
+    }),
+});
--- src/server/trpc/router/shelf.ts
+++ src/server/trpc/router/shelf.ts
@@ -3,65 +3,70 @@ import { router, publicProcedure } from "../trpc";
 
 export const shelfRouter = router({
   getShelf: publicProcedure
-    .input(z.object({
-      name: z.string()
-    })) 
-    .query( ({ctx, input}) => {
+    .input(
+      z.object({
+        name: z.string(),
+      })
+    )
+    .query(({ ctx, input }) => {
       return ctx.prisma.shelf.findUnique({
         where: {
-          name: input.name
+          name: input.name,
         },
         include: {
           books: {
             orderBy: {
-              createdAt: "desc"
-            }
-          }
-        }
-      })
-    }),
-  getNameList: publicProcedure
-    .query( ({ctx}) => {
-      return ctx.prisma.shelf.findMany({
-        where: {
-          userId: ctx.session?.user?.id
+              createdAt: "desc",
+            },
+          },
         },
-        select: {
-          name: true,
-          isDefault: true,
-          _count: {
-            select: {
-              books: true
-            }
-          }
-        },
-      })
+      });
     }),
+  getNameList: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.shelf.findMany({
+      where: {
+        userId: ctx.session?.user?.id,
+      },
+      select: {
+        name: true,
+        isDefault: true,
+        _count: {
+          select: {
+            books: true,
+          },
+        },
+      },
+    });
+  }),
   add: publicProcedure
-    .input(z.object({
-      name: z.string().max(50)
-    })) 
-    .mutation( ({ctx, input}) => {
-      if (ctx.session?.user?.id){
+    .input(
+      z.object({
+        name: z.string().max(50),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      if (ctx.session?.user?.id) {
         return ctx.prisma.shelf.create({
-          data : {
+          data: {
             name: input.name,
-            userId: ctx.session?.user?.id
-          }
-        })
+            userId: ctx.session?.user?.id,
+          },
+        });
       }
     }),
-    delete: publicProcedure
-      .input(z.object({
-        name: z.string()
-      }))
-      .mutation( ({ctx, input}) => {
-        if (ctx.session?.user?.id){
-          return ctx.prisma.shelf.delete({
-            where: {
-              name: input.name,
-            }
-          })
-        }
-      }),
-})
+  delete: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      if (ctx.session?.user?.id) {
+        return ctx.prisma.shelf.delete({
+          where: {
+            name: input.name,
+          },
+        });
+      }
+    }),
+});
